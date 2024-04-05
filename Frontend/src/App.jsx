6bf94f92c75aa05a9cfc26398f40b5a4c34f9ac1
Fausto Savoya <savoyafausto@gmail.com>
2024-04-05 17:23:49 -0300

    ADD Nav

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,11 @@
+import { Nav } from "./components/Nav/Main";
 
 function App() {
-
-
   return (
-    <div className='w-full h-screen grid place-content-center text-white bg-[#333]'>
-      Tuki
+    <div className="w-full h-screen grid place-content-center text-white bg-[#333]">
+      <Nav />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
