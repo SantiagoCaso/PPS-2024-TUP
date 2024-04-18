169e3352e1df83abee26ed8319857651ab31912f
SantiagoCaso <santiagoignaciocaso@gmail.com>
2024-04-18 19:33:53 -0300

    cambio

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,3 @@
-import Register from "./components/Forms/Register/Register";
-import SingIn from "./components/Forms/Sing in/SingIn";
 import { NavBar } from "./components/Nav/NavBar";
 import { NavProvider } from "./context/Nav/NavProvider";
 
@@ -7,7 +5,7 @@ function App() {
   return (
     <div className="w-full font-montserrat bg-BgDark text-light ">
       <NavProvider>
-      <NavBar />
+        <NavBar />
       </NavProvider>
     </div>
   );
