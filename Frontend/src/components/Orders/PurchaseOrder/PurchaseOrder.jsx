845167783f8481bbe14af759d053f83bd012af85
SantiagoCaso <santiagoignaciocaso@gmail.com>
2024-04-23 20:00:18 -0300

    Cart - OrderPurchase - Footer

--- Frontend/src/components/Orders/PurchaseOrder/PurchaseOrder.jsx
+++ Frontend/src/components/Orders/PurchaseOrder/PurchaseOrder.jsx
@@ -112,29 +112,26 @@ const PurchaseOrder = () => {
               <div className="flex justify-between items-start w-full">
                 <div className="flex justify-center items-center space-x-4">
                   <div className="w-8 h-8">
-                    <img
+                    {/* <img
                       className="w-full h-full"
                       alt="logo"
                       src="https://i.ibb.co/L8KSdNQ/image-3.png"
-                    />
+                    /> */}
+                    <p>Logo empresa </p>
                   </div>
                   <div className="flex flex-col justify-start items-center">
                     <p className="text-lg leading-6 font-semibold text-gray-800">
-                      DPD Delivery
-                      <br />
-                      <span className="font-normal">
-                        Delivery with 24 Hours
-                      </span>
+                      Empresa de envio
                     </p>
                   </div>
                 </div>
                 <p className="text-lg font-semibold leading-6 text-gray-800">
-                  $8.00
+                  $Precio de envio
                 </p>
               </div>
               <div className="w-full flex justify-center items-center">
                 <button className="hover:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 py-5 w-96 md:w-full bg-gray-800 text-base font-medium leading-4 text-white">
-                  View Carrier Details
+                  Ver detalles del carrito
                 </button>
               </div>
             </div>
@@ -171,7 +168,6 @@ const PurchaseOrder = () => {
                 <button className="mt-6 md:mt-0 py-5 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 border border-gray-800 font-medium w-96 2xl:w-full text-base leading-4 text-gray-800">
                   Editar Datos
                 </button>
-                <p>CAMBIO PRUEBA</p>
               </div>
             </div>
           </div>
