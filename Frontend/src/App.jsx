6b42a02a8805ca990dec64e32e714e11840915ae
SantiagoCaso <santiagoignaciocaso@gmail.com>
2024-04-25 18:49:27 -0300

    Footer 2.0

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { NavBar } from "./components/Nav/NavBar";
 import { NavProvider } from "./context/Nav/NavProvider";
+import Profile from "./components/User/Profile";
 
 function App() {
   return (
     <div className="w-full font-montserrat ">
       <NavProvider>
         <NavBar />
+        <Profile />
       </NavProvider>
     </div>
   );
