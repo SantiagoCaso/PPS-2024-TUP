f3805a035fcff0c750d6febfc8a5777526497af1
SantiagoCaso <santiagoignaciocaso@gmail.com>
2024-05-01 17:27:03 -0300

    cambio2.0

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import { NavBar } from "./components/Nav/NavBar";
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
 function App() {
   const { pathname } = useLocation();
 
@@ -15,6 +16,7 @@ function App() {
       <div className="customWidth min-h-screen ">
         <Outlet />
       </div>
+      <Footer />
     </div>
   );
 }
