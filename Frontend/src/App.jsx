4389156019ad8e11abcec6d6d161ed2681100859
SantiagoCaso <santiagoignaciocaso@gmail.com>
2024-07-05 19:52:44 -0300

    Cambios back

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,28 +1,21 @@
-<<<<<<< HEAD
 import { Outlet } from "react-router-dom";
 import { NavBar } from "./components/Nav/NavBar";
 import Footer from "./components/Footer/Footer";
 import { ProductsProvider } from "./context/Products/ProductsProvider";
-=======
-import { Outlet } from 'react-router-dom';
-import { NavBar } from './components/Nav/NavBar';
-import Footer from './components/Footer/Footer';
-import { ProductsProvider } from './context/Products/ProductsProvider';
- import FornitureChatBot from './components/shared/Chatbot/FornitureChatbot';
-import { Toaster } from 'sonner';
->>>>>>> dc43c88e591e7668f94a71a7cb8d686e74522357
+import FornitureChatBot from "./components/shared/Chatbot/FornitureChatbot";
+import { Toaster } from "sonner";
 
 function App() {
   return (
     <ProductsProvider>
       <div className="overflow-hidden bg-light-dark">
         <NavBar />
-        <Toaster richColors position="top-center" closeButton  />
+        <Toaster richColors position="top-center" closeButton />
         <div className="customWidth min-h-[calc(100vh-64px)] customPadding  ">
           <Outlet />
         </div>
         <Footer />
-         <FornitureChatBot /> 
+        <FornitureChatBot />
       </div>
     </ProductsProvider>
   );
