c20783b5185167a52a6852d49c0559eb1911dfbf
SantiagoCaso <santiagoignaciocaso@gmail.com>
2024-07-07 19:36:37 -0300

    Close/Open ChatBot

--- Frontend/src/components/Home/Hero/HeroSection.jsx
+++ Frontend/src/components/Home/Hero/HeroSection.jsx
@@ -2,12 +2,12 @@ export const HeroSection = () => {
   return (
     <section className=" relative flex  justify-center px-0 w-screen xl:w-full ">
       <img
-        className=" overflow-hidden xl:rounded-3xl xl:h-[calc(100vh-130px)] xl:mb-11 2xl:w-screen  "
+        className=" overflow-hidden xl:rounded-3xl xl:h-[calc(100vh-130px)] xl:mb-11 2xl:w-screen "
         src="/assets/png/products/hero-img.webp"
         alt=""
       />
       <h1 className="uppercase absolute px-10 text-center top-12 font-[600] max-w-lg md:top-20 lg:top-36 xl:top-48">
-        Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
+        La comodidad en tu hogar
       </h1>
     </section>
   );
--- Frontend/src/components/shared/Chatbot/FornitureChatbot.jsx
+++ Frontend/src/components/shared/Chatbot/FornitureChatbot.jsx
@@ -1,59 +1,75 @@
-import ChatBot from 'react-simple-chatbot';
-import { ChatResponse } from './ChatResponse';
-import { ThemeProvider } from 'styled-components';
-import { useState } from 'react';
+import ChatBot from "react-simple-chatbot";
+import { ChatResponse } from "./ChatResponse";
+import { ThemeProvider } from "styled-components";
+import { useState } from "react";
 const FornitureChatBot = () => {
+  const [chat, setChat] = useState(true);
   const [steps] = useState([
     {
-      id: '1',
+      id: "1",
       message:
-        'Hola! Por favor, ingresa el nombre del producto que estás buscando:',
-      trigger: 'productName',
+        "Hola! Por favor, ingresa el nombre del producto que estás buscando:",
+      trigger: "productName",
     },
     {
-      id: 'productName',
+      id: "productName",
       user: true,
       validator: (value) => {
-        if (value.trim() === '') {
-          return 'Por favor, ingresa un nombre de producto válido.';
+        if (value.trim() === "") {
+          return "Por favor, ingresa un nombre de producto válido.";
         }
         return true;
       },
-      trigger: '3',
+      trigger: "3",
     },
     {
-      id: '3',
-      message: 'Buscando el producto {previousValue}...',
-      trigger: 'searchProduct',
+      id: "3",
+      message: "Buscando el producto {previousValue}...",
+      trigger: "searchProduct",
     },
     {
-      id: 'searchProduct',
+      id: "searchProduct",
       component: <ChatResponse />,
       waitAction: true,
-      trigger: 'end',
+      trigger: "end",
     },
     {
-      id: 'end',
-      message: 'Esa fue nuestra mejor coincidencia!',
+      id: "end",
+      message: "Esa fue nuestra mejor coincidencia!",
       end: true,
     },
   ]);
   const theme = {
-    background: '#f5f8fb',
-    fontFamily: 'Montserrat',
-    headerBgColor: 'black',
-    headerFontColor: '#fff',
-    headerFontSize: '15px',
-    botBubbleColor: 'black',
-    botFontColor: '#fff',
-    userBubbleColor: 'false',
-    userFontColor: 'user: true,',
+    background: "#f5f8fb",
+    fontFamily: "Montserrat",
+    headerBgColor: "black",
+    headerFontColor: "#fff",
+    headerFontSize: "15px",
+    botBubbleColor: "black",
+    botFontColor: "#fff",
+    userBubbleColor: "false",
+    userFontColor: "user: true,",
+  };
+
+  const handlerChat = () => {
+    setChat(!chat);
   };
 
   return (
-    <div className="fixed bottom-5 left-1 w-[100px]  ">
+    <div className="fixed bottom-5 left-1 w-[100px] ">
       <ThemeProvider theme={theme}>
-        <ChatBot steps={steps} headerTitle="Forniture Chat" />
+        {chat ? (
+          <div>
+            <button onClick={handlerChat}>X</button>
+            <ChatBot steps={steps} headerTitle="Forniture Chat" />
+          </div>
+        ) : (
+          <div>
+            <p onClick={handlerChat} className="shadow-xl">
+              Abrir chat
+            </p>
+          </div>
+        )}
       </ThemeProvider>
     </div>
   );
